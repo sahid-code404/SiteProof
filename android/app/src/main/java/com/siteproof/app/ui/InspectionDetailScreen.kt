004c740c8ca7f94b81eff4b45e572a9d9e8cc16c
Sahidul Haque <145923523+sahid-code404@users.noreply.github.com>
2026-08-22 09:06:03 +0530

    fix(android): opt in to Material 3 top app bar API

--- android/app/src/main/java/com/siteproof/app/ui/InspectionDetailScreen.kt
+++ android/app/src/main/java/com/siteproof/app/ui/InspectionDetailScreen.kt
@@ -20,6 +20,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -47,6 +48,7 @@ private fun videoLengthLabel(seconds: Int): String = when {
     else -> "$seconds seconds"
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun InspectionDetailScreen(
     state: InspectionDetailState,
